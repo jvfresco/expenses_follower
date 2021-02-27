46fe450b4930c57fa501eb2ec536b3b7dc24f086
Fispe <javifge@gmail.com>
2021-02-27 13:16:59 +0100

    useAsync and api-client added - Example of usage in categoriespage

--- src/pages/Categories/CategoriesPage.js
+++ src/pages/Categories/CategoriesPage.js
@@ -1,37 +1,32 @@
-import { handleFormSubmit } from '../form_utils/form_utils'
 import CategoryForm from './CategoryForm'
+import {client} from '../../utils/api-client'
+import {useAsync} from '../../utils/hooks'
 
 const CategoriesPage = (props) => {
-
+  const {data, error, run, isLoading, isError, isSuccess} = useAsync()
     const handleFormSubmit = async(data) => {
         const type = props.purpose === 'expense' ? 'expense-category' : 'income-category'
 
-        try {
-            const res = await fetch("http://localhost:8080/categories/" + type, {
-              method: "POST",
-              headers: {
-                  Authorization: "Bearer " + props.token,
-                  "Content-Type": "application/json" 
-              },
-              body: JSON.stringify({
-                  category: data.category,
-              }),
-            });
-  
-            if(res.status === 403){
-              throw new Error("Not authorized")
-            }else if (res.status !== 200 && res.status !== 201) {
-              throw new Error("Creating or editing category failed!");
-            }
-          } catch (err) {
-              console.log(err)
-          }
+        run(client("http://localhost:8080/categories/" + type, {
+          method: "POST",
+          headers: {
+            Authorization: "Bearer " + props.token,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            category: data.category,
+          }),
+        })
+        )
     }
     
     return (
         <div>
             <h1>{props.purpose === 'expense' ? 'Expenses categories' : 'Incomes categories'}</h1>
             <CategoryForm onSubmit={handleFormSubmit}/>
+            {isError ? <p style={{color: 'red'}}>{error.message}</p> : null}
+            {isLoading ? <p style={{color: 'blue'}}>loading...</p> : null}
+            {isSuccess ? <p style={{color: 'green'}}>{data.message}</p> : null}
             <h2>{props.purpose === 'expense' ? 'Expenses categories list' : 'Incomes categories list'}</h2>
         </div>
     )
---
+++ src/utils/api-client.js
@@ -0,0 +1,19 @@
+function client(endpoint, customConfig = {}) {
+    const config = {
+      method: 'GET',
+      ...customConfig,
+    }
+  
+    return window
+      .fetch(endpoint, config)
+      .then(async response => {
+        const data = await response.json()
+        if (response.ok) {
+          return data
+        } else {
+          return Promise.reject(data)
+        }
+      })
+  }
+  
+  export {client}
